791a4cceceaf61a5b7d44a49aa095d2c0f248932
aerits <stev_nm@protonmail.com>
2023-01-06 18:01:09 -0700

    adfsda

---
+++ src/setup.py
@@ -0,0 +1,22 @@
+# this compiles python into a binary i think
+# i will figure it out later
+
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+# "packages": ["os"] is used as example only
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name="guifoo",
+    version="0.1",
+    description="My GUI application!",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("/home/diced/Documents/gembrowse/src/display.py", base=base)],
+)
