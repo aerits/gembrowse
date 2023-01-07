9fdc71f5f4938086e6b079205ea43286993a729c
aerits <stev_nm@protonmail.com>
2023-01-06 18:17:05 -0700

    sdafsf

--- src/setup.py
+++ src/setup.py
@@ -6,7 +6,7 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
 # "packages": ["os"] is used as example only
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+build_exe_options = {"packages": ["os, PyQt5"], "excludes": ["tkinter"]}
 
 # base="Win32GUI" should be used only for Windows GUI app
 base = None
